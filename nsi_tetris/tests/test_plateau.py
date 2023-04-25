dfd02dae2e4bbc1bdca9ca1d10956a3ee22bbfb6
Mathieu VINET <mathieu.vinet>
2023-04-25 13:51:12 +0200

    [WIP] test est_obstruee et test verouiller

--- nsi_tetris/tests/test_plateau.py
+++ nsi_tetris/tests/test_plateau.py
@@ -3,8 +3,10 @@
 import unittest
 from pygame.color import Color
 
+
 from nsi_tetris.jeu.plateau import Plateau, Grille
 from nsi_tetris.jeu.constantes import MODELES_TETRIMINOS
+from nsi_tetris.jeu.tetrimino import Tetrimino
 
 C = Color("white")
 N = None
@@ -87,5 +89,59 @@ class TestEstObstrue(unittest.TestCase):
         with self.assertRaises(TypeError):
             Plateau(5,5).est_obstrue('') #type: ignore            
 
+    def test_fonctionnement(self):
+        """Vérifie que la méthode fonctionne bien"""
+        plateau = Plateau(10,10)
+        tetrimino1 = Tetrimino(MODELES_TETRIMINOS['J'])
+        tetrimino1.set_position(9,0)
+        self.assertTrue(plateau.est_obstrue(tetrimino1))
+        
+        tetrimino1.set_position(0,0)
+        plateau.verrouiller(tetrimino1)
+        tetrimino2 = Tetrimino(MODELES_TETRIMINOS['L'])
+        self.assertTrue(plateau.est_obstrue(tetrimino2))
+
+class TestVerrouiller(unittest.TestCase):
+    """Tests de la méthode verouiller"""
+
+    def test_erreurs(self):
+        """Vérifie que la méthode lève les bonnes erreurs"""
+        with self.assertRaises(TypeError):
+            Plateau(5,5).verrouiller('') #type: ignore
+
+    def test_fonctionnement(self):
+        """Vérifie que la méthode fonctionne bien"""
+        plateau = Plateau(10,10)
+        tetrimino = Tetrimino(MODELES_TETRIMINOS['Z'])
+        plateau.verrouiller(tetrimino)
+        self.assertEqual(plateau.grille()[0][0],Color('red'))
+
+class TestEffacerLigne(unittest.TestCase):
+    """Tests de la méthode effacer_ligne"""
+    def test_erreurs(self):
+        """Vérifie que la méthode lève les bonnes erreurs"""
+        with self.assertRaises(TypeError):
+            Plateau(5,5).effacer_ligne('') #type: ignore
+        
+        with self.assertRaises(ValueError):
+            Plateau(5,5).effacer_ligne(-1)
+        
+        with self.assertRaises(ValueError):
+            Plateau(5,5).effacer_ligne(20)
+
+    def test_fonctionnement(self):
+        plateau = depuis_grille([
+            [C,C,N,C],
+            [N,C,C,N],
+            [C,N,N,C],
+            [C,C,C,C]
+        ])
+        plateau.effacer_ligne(2)
+        self.assertEqual(plateau.grille(),(
+            (N,N,N,N),
+            (C,C,N,C),
+            (N,C,C,N),
+            (C,C,C,C)
+        ))
 if __name__ == "__main__":
     unittest.main()
